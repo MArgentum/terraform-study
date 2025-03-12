a3150c1e22be4eb61ef6fee02bf28b64c7ad7a0d
Matvey <matvevait5686@gmail.com>
2025-03-12 03:09:23 +0200

    add 1000 unit-test

--- infra.sh
+++ infra.sh
@@ -2,8 +2,17 @@
 
 set -o xtrace
 
+terraform validate
+
 terraform apply -auto-approve
 
-PUBLIC_IP=$(terraform output -raw public_ip)
+alb_dns_name=$(terraform output -raw alb_dns_name)
 
-curl http://${PUBLIC_IP}:8080
+for i in {1..1000}; do
+    if ! curl -s -o /dev/null http://${alb_dns_name}:8080; then
+        echo "Ошибка при выполнении запроса номер $i"
+        exit 1
+    fi
+done
+
+echo "Все ок"
